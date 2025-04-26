147f58f58b0c67dacc21576d76347133194e68c6
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-04-26 22:10:37 +0200

    chore: update dependencies and improve login form structure
    - Added @apollo/client dependency and updated graphql version in package.json.
    - Modified CircleBox.module.css to use hsl for color definition.
    - Enhanced login page layout with improved form handling using React Hook Form.
    - Adjusted styles for better responsiveness and user experience in the login section.

--- src/app/(teacher)/teacher/login/page.tsx
+++ src/app/(teacher)/teacher/login/page.tsx
@@ -1,49 +1,54 @@
 'use client'
-import React from 'react'
-import styles from './page.module.css'
-import { Input } from '../../../../components/ui/input'
-import { Button } from '../../../../components/ui/button'
-import { TextField } from '@payloadcms/ui'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Email } from '../../../../blocks/Form/Email'
-import { useForm } from 'react-hook-form'
 import { Text } from '../../../../blocks/Form/Text'
+import { Button } from '../../../../components/ui/button'
+import styles from './page.module.css'
 const LoginTeacher = () => {
+  const methods = useForm()
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = methods
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
   return (
     <section className={styles.loginSection}>
-      <div className={styles.card}>
-        <div className={styles.cardHeader}>
-          <h2>先生方のログイン</h2>
-          <img src="/images/migla-logo-square.png" height={100} width={100} alt="logo" />
-        </div>
-        <div className={styles.inputSection}>
-          <Email
-            blockType="email"
-            name="email"
-            label="メールアドレス"
-            required
-            width={100}
-            register={register}
-            errors={errors}
-          />
-          <Text
-            blockType=""
-            name="password"
-            label="パスワード"
-            required
-            width={100}
-            register={register}
-            errors={errors}
-          />
-        </div>
-        <Button className={styles.loginButton} variant="default">
-          ログイン
-        </Button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormProvider {...methods}>
+          <div className={styles.card}>
+            <div className={styles.cardHeader}>
+              <h2>先生方のログイン</h2>
+              <img src="/images/migla-logo-square.png" height={100} width={100} alt="logo" />
+            </div>
+            <div className={styles.inputSection}>
+              <Email
+                blockType="email"
+                name="email"
+                label="メールアドレス"
+                required
+                register={register}
+                errors={errors}
+                width={80}
+              />
+              <Text
+                blockType="text"
+                name="password"
+                label="パスワード"
+                required
+                register={register}
+                errors={errors}
+                width={80}
+              />
+            </div>
+            <Button className={styles.loginButton} variant="default" type="submit">
+              ログイン
+            </Button>
+          </div>
+        </FormProvider>
+      </form>
     </section>
   )
 }
