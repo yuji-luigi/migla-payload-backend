9c3bba5c0015120cb082210bc7e209a37b484164
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-05-10 14:16:36 +0200

    feat: seed file

---
+++ src/tasks/seed.ts
@@ -0,0 +1,52 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+
+const seed = async () => {
+  // Get a local copy of Payload by passing your config
+  const payload = await getPayload({ config })
+
+  const superAdmin = await payload.create({
+    collection: 'roles',
+    // locale: 'ja',
+    // fallbackLocale: 'ja',
+    data: {
+      name: 'super_admin',
+      slug: 'super_admin',
+      label: 'スーパー管理者',
+    },
+  })
+
+  await payload.create({
+    collection: 'roles',
+    // locale: 'ja',
+    // fallbackLocale: 'ja',
+    data: {
+      name: 'admin',
+      slug: 'admin',
+      label: '管理者',
+    },
+  })
+  await payload.create({
+    collection: 'roles',
+    // locale: 'ja',
+    // fallbackLocale: 'ja',
+    data: {
+      name: 'teacher',
+      slug: 'teacher',
+      label: '先生',
+    },
+  })
+  await payload.create({
+    collection: 'roles',
+    // locale: 'ja',
+    // fallbackLocale: 'ja',
+    data: {
+      name: 'parent',
+      slug: 'parent',
+      label: '保護者',
+    },
+  })
+}
+
+// Call the function here to run your seed script
+await seed()
