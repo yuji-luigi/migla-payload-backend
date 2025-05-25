cce51c87d087e108a01b4c46b3fcb062a7f6acf2
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-05-25 07:33:36 +0200

    fix: hide import button in create edit page

--- src/collections/Users/ui/DescriptionUsers.tsx
+++ src/collections/Users/ui/DescriptionUsers.tsx
@@ -3,10 +3,20 @@ import React from 'react'
 import { TailWindScope } from '../../../components/tailwind/TailWindScope'
 import { Button, useModal } from '@payloadcms/ui'
 import { useCustomTranslations } from '../../../lib/i18n/useCustomTranslations'
+import { useParams, usePathname } from 'next/navigation'
 
 const DescriptionUsers = ({ slug }: { slug: string }) => {
   const { openModal } = useModal()
+  const pathname = usePathname()
+  const params = useParams()
+  const isListPage = pathname
+    .split('/')
+    .pop()
+    ?.startsWith(slug as string)
   const { t } = useCustomTranslations()
+  if (!isListPage) {
+    return null
+  }
   return (
     <TailWindScope className="w-full flex justify-end">
       <Button onClick={() => openModal('users')}>{t('button:Import')}</Button>
