4f989558342479349b275a0eaa1e7e5e5e3551e4
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-06-06 21:54:45 +0200

    refactor: enhance Tailwind CSS configuration and utility classes
    - Updated `tailwind.config.mjs` to include additional file types in the content array for better utility class recognition.
    - Added new utility classes for background and text colors in `globals.css`.
    - Modified header component to improve layout and structure.
    - Cleaned up CSS imports in teacher-specific stylesheets for consistency.

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -28,13 +28,13 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [headerTheme])
-
   return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
+
         <HeaderNav data={data} />
       </div>
     </header>
