22eff73b77d04d0e01867b7859b594e6f3031152
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-06-10 23:17:28 +0200

    refactor: enhance AdminBeforeDashboard component
    - Added localization support for the "no classrooms" message in the AdminBeforeDashboard component.
    - Updated the component structure to improve clarity and maintainability, including type casting for translation functions.

--- src/components/BeforeDashboard/AdminBeforeDashboard.tsx
+++ src/components/BeforeDashboard/AdminBeforeDashboard.tsx
@@ -1,4 +1,6 @@
+import { TFunction } from '@payloadcms/translations'
 import { ServerProps } from 'payload'
+import { CustomTranslationsKeys } from '../../lib/i18n/i18n_configs'
 
 export const AdminBeforeDashboard = async (props: ServerProps) => {
   const components: React.ReactNode[] = []
@@ -8,11 +10,13 @@ export const AdminBeforeDashboard = async (props: ServerProps) => {
   })
 
   if (pagClassrooms.totalDocs === 0) {
-    components.push(<div key="no-classrooms"></div>)
+    components.push(<div key="no-classrooms">There are no classrooms</div>)
   }
   return (
-    <section className="admin-before-dashboard dashboard__group">
-      <h2 className="dashboard__label">{props.i18n.t('dashboard:setup')}</h2>
+    <section className="admin-before-dashboard">
+      <h2 className="dashboard__label">
+        {(props.i18n.t as unknown as TFunction<CustomTranslationsKeys>)('dashboard:setup')}
+      </h2>
       {components.map((component) => component)}
     </section>
   )
