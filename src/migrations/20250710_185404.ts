0a6d9c92257edaf28ca1d70c76c450e5c2e584e2
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-07-10 21:35:33 +0200

    fix: migration

--- src/migrations/20250710_185404.ts
+++ src/migrations/20250710_185404.ts
@@ -2,6 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
+  CREATE TYPE "public"."_locales" AS ENUM('ja', 'en', 'it');
   CREATE TYPE "public"."enum_pages_hero_links_link_type" AS ENUM('reference', 'custom');
   CREATE TYPE "public"."enum_pages_hero_links_link_appearance" AS ENUM('default', 'outline');
   CREATE TYPE "public"."enum_pages_blocks_cta_links_link_type" AS ENUM('reference', 'custom');
