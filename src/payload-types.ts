d80358d32e606fed35d3ccd8be66412bc9d1b127
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-07-11 22:43:12 +0200

    fix: type Media createdBy nullable

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -282,7 +282,7 @@ export interface Post {
 export interface Media {
   id: number;
   alt?: string | null;
-  createdBy: number | User;
+  createdBy?: (number | null) | User;
   caption?: {
     root: {
       type: string;
@@ -394,6 +394,13 @@ export interface User {
   hash?: string | null;
   loginAttempts?: number | null;
   lockUntil?: string | null;
+  sessions?:
+    | {
+        id: string;
+        createdAt?: string | null;
+        expiresAt: string;
+      }[]
+    | null;
   password?: string | null;
 }
 /**
@@ -1433,6 +1440,13 @@ export interface UsersSelect<T extends boolean = true> {
   hash?: T;
   loginAttempts?: T;
   lockUntil?: T;
+  sessions?:
+    | T
+    | {
+        id?: T;
+        createdAt?: T;
+        expiresAt?: T;
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
