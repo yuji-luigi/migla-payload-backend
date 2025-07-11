24d7e868b4ab5e69df7fb60abf1d0a9809cac26e
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-07-11 23:20:04 +0200

    fix: migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250710_185404 from './20250710_185404';
-import * as migration_20250711_211419 from './20250711_211419';
+import * as migration_20250711_211942 from './20250711_211942';
 
 export const migrations = [
   {
-    up: migration_20250710_185404.up,
-    down: migration_20250710_185404.down,
-    name: '20250710_185404',
-  },
-  {
-    up: migration_20250711_211419.up,
-    down: migration_20250711_211419.down,
-    name: '20250711_211419'
+    up: migration_20250711_211942.up,
+    down: migration_20250711_211942.down,
+    name: '20250711_211942'
   },
 ];
