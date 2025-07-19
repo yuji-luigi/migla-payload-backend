5d4102221e263057cf72cf51c9bfb8ad0e99b753
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-07-19 20:12:48 +0200

    refactor: send push notification with generic function

--- src/collections/notificaitons/hooks/afterChangeHookNotification.ts
+++ src/collections/notificaitons/hooks/afterChangeHookNotification.ts
@@ -1,39 +1,25 @@
 import { getMessaging } from 'firebase-admin/messaging'
 import { CollectionAfterChangeHook } from 'payload'
 import { Notification } from '../../../payload-types'
+import { sendPushNotificationsForEach } from '../../../configs/firebase/sendPushNotificationsForEach'
 
 const handleSendNotification: CollectionAfterChangeHook<Notification> = async ({
   operation,
   req,
   doc,
+  collection,
 }) => {
-  // const { docs: users } = await req.payload.find({
-  //   collection: 'users',
-  //   where: {
-  //     fcmToken: {
-  //       exists: true,
-  //     },
-  //   },
-  // })
   const { docs: fcmTokens } = await req.payload.find({
     collection: 'fcmTokens',
   })
-  getMessaging()
-    .sendEachForMulticast({
-      tokens: fcmTokens
-        .map((tokenData) => tokenData.token)
-        .filter((token) => typeof token === 'string'),
-      notification: {
-        title: doc.title,
-        body: doc.body,
-      },
-    })
-    .then((response) => {
-      console.log('Successfully sent message:', response)
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error)
-    })
+  sendPushNotificationsForEach({
+    payload: req.payload,
+    fcmTokens,
+    title: doc.title,
+    body: doc.body,
+    collection: collection.slug,
+    type: doc.type,
+  })
 }
 
 export const afterChangeNotification: CollectionAfterChangeHook<Notification>[] = [
