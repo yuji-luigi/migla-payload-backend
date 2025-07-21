333a8118f8a62173fd238faf26b12e44d0d1b9e7
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-07-21 06:44:49 +0200

    feat(reports collection): eliminate readRecord in response

--- src/collections/reports/hooks/reportHooks.ts
+++ src/collections/reports/hooks/reportHooks.ts
@@ -1,8 +1,11 @@
 import { CollectionConfig } from 'payload'
 import { teacherOperationBeforeChange } from './teacheRecordsBeforeChange'
 import { sendPushNotificationAfterChangeReport } from './sendPushNotificationAfterChangeReport'
+import { createReadReport } from './createReadReport'
+import { deleteReadRecordFieldReport } from './deleteReadReportInResponse'
 
 export const reportHooks: CollectionConfig<'reports'>['hooks'] = {
   beforeChange: [teacherOperationBeforeChange],
   afterChange: [sendPushNotificationAfterChangeReport],
+  afterRead: [createReadReport, deleteReadRecordFieldReport],
 }
