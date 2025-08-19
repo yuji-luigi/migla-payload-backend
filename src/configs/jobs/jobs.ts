eb39234a9c0bbfc45306754504a908d6be554fde
Yuji Sato <yujisato.usk.jpn@gmail.com>
2025-08-19 21:50:40 +0200

    feat: add Vercel cron configuration for scheduled job execution and enhance user role checks for access control

--- src/configs/jobs/jobs.ts
+++ src/configs/jobs/jobs.ts
@@ -1,3 +1,4 @@
+import { currentUserFilter } from './../../access/filters/currentUserFilterNonAdmin'
 import { JobsConfig, PayloadRequest, TaskConfig } from 'payload'
 import { sendScheduledPaymentNotificationQueue } from './tasks/sendScheduledPaymentNotificationQueue'
 import { isAboveAdmin, isSuperAdmin } from '../../hooks/showOnlyAdmin'
@@ -7,24 +8,27 @@ export const jobs: JobsConfig = {
   access: {
     run: ({ req }: { req: PayloadRequest }): boolean => {
       // Allow logged in users to execute this endpoint (default)
-      if (req.user) return true
+      console.log(req.user)
+      if (req.user?.currentRole?.isAdmin || req.user?.currentRole?.isSuperAdmin) return true
 
       // If there is no logged in user, then check
       // for the Vercel Cron secret to be present as an
       // Authorization header:
       const authHeader = req.headers.get('authorization')
+      console.log('authHeader')
+      console.log(authHeader === `Bearer ${process.env.CRON_SECRET}`)
       return authHeader === `Bearer ${process.env.CRON_SECRET}`
     },
   },
-  autoRun: [
-    {
-      //every 5 seconds
-      cron: '*/5 * * * * *',
-      limit: 10, // limit jobs to process each run
-      queue: 'every-five-seconds', // name of the queue
-    },
-    // add as many cron jobs as you want
-  ],
+  // autoRun: [
+  //   {
+  //     //every 5 seconds
+  //     cron: '*/5 * * * * *',
+  //     limit: 10, // limit jobs to process each run
+  //     queue: 'every-five-seconds', // name of the queue
+  //   },
+  //   // add as many cron jobs as you want
+  // ],
   tasks: [sendScheduledPaymentNotificationQueue],
   jobsCollectionOverrides: ({ defaultJobsCollection }) => {
     if (!defaultJobsCollection.admin) {
